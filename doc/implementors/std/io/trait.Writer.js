83e47ed1e22d1eaf57b623cc1b93742940ff2825
Jim OBrien <jimobrien930@gmail.com>
2014-10-09 16:29:49 -0700

    doc update

---
+++ doc/implementors/std/io/trait.Writer.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['memstream'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='http://doc.rust-lang.org/master/std/io/trait.Writer.html' title='std::io::Writer'>Writer</a> for <a class='struct' href='memstream/struct.MemStream.html' title='memstream::MemStream'>MemStream</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
